df50c24193f9bee73b7f79696e29f43fad942faf
BrucieJack <nikitos17082002@mail.ru>
2022-03-20 00:04:54 +0300

    init

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyController extends Controller
+{
+    public function myPage(){
+        echo "<h1>Bliznets Nikita Vladimirovich</h1>";
+    }
+}
